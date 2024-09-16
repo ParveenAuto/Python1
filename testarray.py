c992d6437930076c715e253a8b3a266e2776a7c5
PD240891 <parvndh08@gmail.com>
2024-09-16 13:26:01 +0100

    nhfhn

--- testarray.py
+++ testarray.py
@@ -1,5 +1,7 @@
 #take a list with with combination of 10 alaphabet and numbers and print all data one by one starting from 2nd no to second last number
 
 mylist=("a", 3, 5, 6, 9, 90, "g", "r", "p", 7)
-print(mylist[1:10])
+for i in range(1, len(mylist)-1):
+    #for x in mylist(range(1,9)):
+    print(mylist[i])
 
