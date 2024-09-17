c7c57098ec607772cf804acfe5d67890291d2f44
PD240891 <parvndh08@gmail.com>
2024-09-17 11:44:52 +0100

    fd

--- testarray.py
+++ testarray.py
@@ -18,10 +18,17 @@
 #Que3 Given an array, arr of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
 #Solution
 
-def findelement(arr):
-    for i in range(len(arr)):
-        if arr[i] == x:
-            return i
+# def findelement(arr, x):
+#     for i in range(len(arr)):
+#         if arr[i] == x:
+#             return i
+# arr=[1, 8, 9, 10]
+# x=int(input("enter the number"))
+
+# y= findelement(arr, x)
+# print(y)
+
+
     
         
 
