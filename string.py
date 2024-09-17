0e4d1d1692ca236d452e71ea4652d8dd6ffaf025
PD240891 <parvndh08@gmail.com>
2024-09-17 12:23:48 +0100

    k

--- string.py
+++
@@ -1,7 +0,0 @@
-#Que Reverse string
-
-def myfunction(value):
-    return reversed(value)
-value=str(input("enter the word"))
-deploy=my function(value)
-print(deploy)
--- teststri.py
+++ teststri.py
@@ -2,6 +2,10 @@
 
 def reverse_string_loop(input_str):
     reversed_str=""
-    for char in input_str[::-1]
-    reversed_str += char
+    for char in input_str[::-1]:
+        reversed_str += char
     return reversed_str
+input_str=str(input("enter the value"))
+reverse1=reverse_string_loop(input_str)
+print(reverse1)
+
