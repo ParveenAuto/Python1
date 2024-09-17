1b3fb44d4bf106729c28aeb6fb7b5fa2e9b1f5ed
PD240891 <parvndh08@gmail.com>
2024-09-17 10:44:16 +0100

    gh

--- testarray.py
+++ testarray.py
@@ -1,7 +1,42 @@
-#take a list with with combination of 10 alaphabet and numbers and print all data one by one starting from 2nd no to second last number
+#Que1 take a list with with combination of 10 alaphabet and numbers and print all data one by one starting from 2nd no to second last number
+#Solution
+# mylist=("a", 3, 5, 6, 9, 90, "g", "r", "p", 7)
+# for i in range(1, len(mylist)-1):
+#     print(mylist[i])
 
-mylist=("a", 3, 5, 6, 9, 90, "g", "r", "p", 7)
-for i in range(1, len(mylist)-1):
-    #for x in mylist(range(1,9)):
-    print(mylist[i])
+#Que2 Given two arrays arr1[] and arr2[], the task is to find the number of elements in the union between these two arrays.
+#Solution
+# arr1=set([1,3,87,98,76])
+# arr2=set([67,45,98,87,901,1,0])
+
+# union = arr1.union(arr2)
+# intersection=arr1.intersection(arr2)
+
+# print(union)
+# print(intersection)
+
+#Que3 Given an array, arr of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+#Solution
+number=input("Enter the number")
+arr1 =[1,2,3,4]
+if number==1:
+    print(0)
+elif number==2:
+    print(1)
+elif number==3:
+    print(2)
+else:
+    print(3)
+        
+
+
+
+    
+
+
+
+
+
+
+
 
