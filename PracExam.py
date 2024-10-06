41dccb95ac4f0efb21b798f291129d0654ca913d
PD240891 <parvndh08@gmail.com>
2024-10-06 15:58:44 +0100

    d

--- PracExam.py
+++ PracExam.py
@@ -7,11 +7,20 @@
 
 # print (sum_value)
 
-cast=["a", "b"]
-cast.extend(["c", "d"])
+#Que 2. Extending a list
+# cast=["a", "b"]
+# cast.extend(["c", "d"])
 
-print(cast)
+# print(cast)
 
+#Que 3. Life weekd left.
 
+def life_in_weeks():
+    age = int(input("Enter your age:"))
+    years_left = 90 - age
+    weeks_left = years_left * 52 
+    
 
+    print(f"You have {weeks_left} weeks left.")
 
+life_in_weeks()
