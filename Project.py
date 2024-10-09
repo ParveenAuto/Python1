bb69cd3ac3a8444fe8ae595b9c4ff8cb37f1bf83
PD240891 <parvndh08@gmail.com>
2024-10-09 21:20:12 +0100

    Projct

--- Project.py
+++ Project.py
@@ -1,15 +1,18 @@
 # Develop calculator
 def add(n1, n2):
-    if symbol == +
-    return n1 + n2
-def subtract(n1, n2):
-    return n1 - n2
-def multiply(n1, n2):
-    return n1 * n2
-def divide(n1, n2):
-    return n1/n2
+    if symbol == '+':
+        return n1 + n2
+#def subtract(n1, n2):
+    elif symbol == '-':
+        return n1 - n2
+#def multiply(n1, n2):
+    elif symbol == '*':
+        return n1 * n2
+#def divide(n1, n2):
+    else:
+        return n1/n2
 
-symbol=input("Which operator you want to run? +, -, *, /")
+symbol=input("Which operator you want to run? +, -, *, / ")
 n1=int(input("enter the number"))
 n2=int(input("enter the number"))
 
