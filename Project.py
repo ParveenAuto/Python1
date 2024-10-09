72b4fdb827e4c343008b494d6b2133e17aad4e56
PD240891 <parvndh08@gmail.com>
2024-10-09 21:00:27 +0100

    Project

---
+++ Project.py
@@ -0,0 +1,15 @@
+# Develop calculator
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1/n2
+
+n1=int(input("enter the number"))
+n2=int(input("enter the number"))
+
+print(f"Your output is {add(n1, n2)}")
+
