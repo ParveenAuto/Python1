38540108eadcbd1a46e0e63674cbf8101053c0c9
PD240891 <parvndh08@gmail.com>
2025-03-27 20:26:48 +0000

    updated code

--- RES4CITY_Project/code.py
+++ RES4CITY_Project/code.py
@@ -92,8 +92,17 @@ merged_df = merged_df[desired_column_order]
 # Drop the course_id column
 merged_df.drop(columns=["course_id"], inplace=True)
 
-# Write to a new sheet inside the same Excel file
-with pd.ExcelWriter(file_path, engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
-    merged_df.to_excel(writer, sheet_name="final_data", index=False)
+# # Write to a new sheet inside the same Excel file
+# with pd.ExcelWriter(file_path, engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
+#     merged_df.to_excel(writer, sheet_name="final_data", index=False)
+    
+# Define new file path
+new_file_path = "/Users/pd/Desktop/python/RES4CITY_Project/Cleaned_data.xlsx"
+
+# Save DataFrame to a new Excel file
+merged_df.to_excel(new_file_path, sheet_name="Cleaned_Data", index=False, engine="openpyxl")
+
+print(f"Data successfully saved to: {new_file_path}")
+
 
 print("Data successfully saved to 'final_data'.")
